43d048772d758a0293fe5de2ce2b8e78056c8633
PauloRoberto <paulo.rosantos@rede.ulbra.br>
2026-02-04 19:59:31 -0300

    preparing deploy

--- diagnoVet/main.py
+++ diagnoVet/main.py
@@ -1,5 +1,19 @@
+import os
 from fastapi import FastAPI
 from api.routes import router
 
-app = FastAPI()
-app.include_router(router)
+app = FastAPI(title="diagnoVet API")
+
+app.include_router(router, prefix="/api")
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=True
+    )
